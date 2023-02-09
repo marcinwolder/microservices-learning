55e2a1725953e40f7833aaf957998f2c360f25b7
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-02-09 23:02:28 +0100

    mysql depl

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,17 +1,16 @@
-import mongoose from 'mongoose';
+import { createConnection } from 'mysql';
 import { app } from './app';
 
 (async () => {
 	if (!process.env.JWT_KEY) throw new Error('no JWT_KEY env connected');
-	if (!process.env.MONGO_URL) throw new Error('no MONGO_URL env connected');
 
-	try {
-		mongoose.set('strictQuery', false);
-		await mongoose.connect(process.env.MONGO_URL);
-		console.log('DB connected!');
-	} catch (err) {
-		console.error('DB error');
-	}
+	const link = createConnection('http://tickets-mysql-ip-srv:3306');
+
+	link.connect(function (err) {
+		if (err) throw err;
+		console.log('Connected!');
+	});
+
 	app.listen(4000, () => {
 		console.log('server started on port 4000');
 	});
